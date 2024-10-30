a08806e3d356af842c00e3cf4951370976417dfc
Maria <maria.cccandido@gmail.com>
2024-10-30 11:43:50 -0300

    Add files via upload

---
+++ portifolio.js
@@ -0,0 +1,36 @@
+const checkbox = document.getElementById("check");
+const navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar a ");
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+
+/* ScrollReveal */
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".conteudo", { duration: 1000 });
+
+/* funcao para responsivo */
+
+checkbox.addEventListener("change", function () {
+  if (checkbox.checked) {
+    navbar.style.height = "17.7rem";
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  } else {
+    navbar.style.height = 0;
+    menuIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  }
+});
+
+/* Fucao para quando apertar nos links, fechar o menu e mudar ícone*/
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    const navbar = document.querySelector(".navbar");
+    navbar.style.height = "0";
+    menuIcon.style.display = "block";
+    closeIcon.style.display = "none";
+    checkbox.checked = false;
+  });
+});
